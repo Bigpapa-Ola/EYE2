95cfd90952c2e03d80c3f1580f3b5f9f18df0fe6
ElricOonWW <138241749+ElricOonWW@users.noreply.github.com>
2023-07-01 15:09:28 +0800

    Update main.py
    Main changed

--- main.py
+++ main.py
@@ -10,20 +10,51 @@ CLASS_NAMES = ['Cataracts', 'Diabetic Retinopathy', 'Glaucoma', 'Normal']
 
 def main():
     st.title('EYE DISEASE CLASSIFICATION')
+
+    with st.expander("Instruction"):
+        st.markdown("## Description")
+        st.markdown("""
+        - This is an eye disease classification system that allows users to upload retinal images and perform predictions.
+        - The system can predict the following classes: Cataracts, Diabetic Retinopathy, Glaucoma, and Normal.
+        """)
+
+        st.markdown("## Procedure")
+        st.markdown("""
+        1. Click the "Upload Image" button to upload a retinal image.
+        2. Click the "Predict Eye Disease" button to start the eye disease prediction.
+        3. Please wait for a few moments to get the result.
+        4. The result will be displayed below the image.
+        """)
+
+        st.markdown("## Accuracy")
+        st.markdown("""
+        - <p><span style="color: Green;"><strong> Green </strong></span> -- Indicate strong confidence</p>
+        - <p><span style="color: Red;"><strong> Red </strong></span> -- Indicate weak confidence</p>
+        """, unsafe_allow_html=True)
+
+
     model = load_model()
     image = load_image()
     result = st.button('PREDICT EYE DISEASE')
+
+
+
     if result:
-        st.write('Calculating results...')
+
+        result_placeholder = st.empty()
+        result_placeholder.write('Calculating results...')
 
         predicted_class, accuracy_percentage = predict(model, CLASS_NAMES, image)
 
-        # if accuracy_percentage >= 0.80:
-        st.sidebar.write('Predicted Class:', predicted_class)
-        st.sidebar.write('Probability: ', accuracy_percentage,'%')
-        # else:
-        #     st.sidebar.write('**Predicted Class: **', predicted_class)
-        #     st.sidebar.write('**Probability: **', accuracy_percentage,'%')
+        result_placeholder.empty() 
+
+        if predicted_class and accuracy_percentage:
+            st.markdown(f'<p><strong>Predicted Class:</strong> {predicted_class}</p>', unsafe_allow_html=True)
+            if accuracy_percentage >= 0.80:
+                st.markdown(f'<p><strong>Accuracy:</strong> <span style="color: Green;"><strong>{accuracy_percentage}%</strong></span></p>', unsafe_allow_html=True)
+            else:
+                st.markdown(f'<p><strong>Accuracy:</strong> <span style="color: Red;"><strong>{accuracy_percentage}%</strong></span></p>', unsafe_allow_html=True)
+
 
 
 
@@ -78,110 +109,3 @@ def predict(model, class_names, image):
 
 if __name__ == '__main__':
     main()
-
-# st.title("EYE DISEASE CLASSIFICATION")
-# st.write('\n')
-
-# MODEL = tf.keras.models.load_model("./model/VGG19/model.epoch06-loss0.34.h5")
-
-# image = Image.open('images/10007_right.jpeg')
-# show = st.image(image, use_column_width=True)
-
-# st.sidebar.title("Upload Image")
-
-
-# #Disabling warning
-# st.set_option('deprecation.showfileUploaderEncoding', False)
-# #Choose your own image
-# uploaded_file = st.sidebar.file_uploader(" ",type=['png', 'jpg', 'jpeg'] )
-
-# if uploaded_file is not None:
-#     u_img = Image.open(uploaded_file)
-#     show.image(u_img, 'Uploaded Image', use_column_width=True)
-    
-#     image = np.asarray(u_img)/255
-#     my_image= np.resize(image, (224,224))
-
-
-
-# # For newline
-# st.sidebar.write('\n')
-    
-# if st.sidebar.button("Click Here to Classify"):
-    
-#     if uploaded_file is None:
-        
-#         st.sidebar.write("Please upload an Image to Classify")
-    
-#     else:
-        
-#         with st.spinner('Classifying ...'):
-            
-#             prediction = predict(cat_clf["w"], cat_clf["b"], my_image)
-#             time.sleep(2)
-#             st.success('Done!')
-            
-#         st.sidebar.header("Algorithm Predicts: ")
-        
-#         probability = "{:.3f}".format(float(prediction*100))
-        
-        
-#         if prediction > 0.5:
-#             st.sidebar.write("It's a 'Cat' picture.", '\n' )
-#             st.sidebar.write('**Probability: **',probability,'%')
-#             st.sidebar.audio(audio_bytes)
-                             
-#         else:
-#             st.sidebar.write(" It's a 'Non-Cat' picture ",'\n')
-#             st.sidebar.write('**Probability: **',probability,'%')
-    
-    
-    
-
-
-# @st.cache(allow_output_mutation=True)
-# def get_base64_of_bin_file(bin_file):
-#     with open(bin_file, 'rb') as f:
-#         data = f.read()
-#     return base64.b64encode(data).decode()
-
-# def set_png_as_page_bg(png_file):
-#     bin_str = get_base64_of_bin_file(png_file) 
-#     page_bg_img = '''
-#     <style>
-#     .stApp {
-#     background-image: url("data:image/png;base64,%s");
-#     background-size: cover;
-#     background-repeat: no-repeat;
-#     background-attachment: scroll; # doesn't work
-#     }
-#     </style>
-#     ''' % bin_str
-    
-#     st.markdown(page_bg_img, unsafe_allow_html=True)
-#     return
-
-# # set_png_as_page_bg('/content/background.webp')
-
-# upload= st.file_uploader('Insert image for classification', type=['png','jpg'])
-# c1, c2= st.columns(2)
-# if upload is not None:
-#     im= Image.open(upload)
-#     img= np.asarray(im)
-#     image= cv2.resize(img,(224, 224))
-#     img= preprocess_input(image)
-#     img= np.expand_dims(img, 0)
-#     c1.header('Input Image')
-#     c1.image(im)
-#     c1.write(img.shape)
-
-
-# #load weights of the trained model.
-# vgg_model.load_weights('')
-
-# # prediction on model
-# vgg_preds = vgg_model.predict(img)
-# vgg_pred_classes = np.argmax(vgg_preds, axis=1)
-# c2.header('Output')
-# c2.subheader('Predicted class :')
-# c2.write(classes[vgg_pred_classes[0]] )
